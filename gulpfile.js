b207f223f55b41d4e092da02bb943c04d3822042
= <=>
2020-12-03 17:06:00 -0500

    Add tunel for external access to website

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,25 @@ const browserSync = require('browser-sync').create();
 // Compile SCSS to CSS
 function style() {
   // 1. Locate SCSS file
-  return gulp.src('./scss/**/*.scss')
-    // 2. Pass that file through the SASS compiler
-    .pipe(sass().on('error', sass.logError))
-    // 3. Location to save the compiled CSS
-    .pipe(gulp.dest('./dist/css/'))
-    // 4. Stream changes to browser
-    .pipe(browserSync.stream());
+  return (
+    gulp
+      .src('./scss/**/*.scss')
+      // 2. Pass that file through the SASS compiler
+      .pipe(sass().on('error', sass.logError))
+      // 3. Location to save the compiled CSS
+      .pipe(gulp.dest('./dist/css/'))
+      // 4. Stream changes to browser
+      .pipe(browserSync.stream())
+  );
 }
 
 function watch() {
   browserSync.init({
     server: {
       baseDir: './'
-    }
+    },
+    online: true,
+    tunnel: true
   });
 
   // On file change run style()
